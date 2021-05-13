3c55462b9bc426378342c46f599e9bfcb0efe8de
Xwin <47206910+Xwin28@users.noreply.github.com>
2021-05-13 12:43:49 +0700

    update Effect

---
+++ Assets/1_Effect/PinwheelFantasyEffect/Script/Spin.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Spin : MonoBehaviour {
+    public float turnDegPerSec = 360;
+    public Vector3 localAxis;
+    protected float turnDegPerFrame;
+
+    public void Start()
+    {
+        turnDegPerFrame = turnDegPerSec * 0.02f;
+
+    }
+
+    public void Update()
+    {
+        
+        transform.Rotate(localAxis, turnDegPerFrame);
+    }
+
+
+
+
+}
